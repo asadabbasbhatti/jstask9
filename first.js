8227ce53269b6f93d4cadae3352beee6e353f65d
asad <asadybrian@gmail.com>
2025-07-30 21:58:21 +0500

    initial commit

--- first.js
+++ first.js
@@ -1,7 +1,8 @@
-const profile={
-    name:"asad",
-    followers:20,
-    rating:4,
-    isFollow:false,
+let num=prompt("enter a number");
+if (num%5==0)
+{
+    console.log(num,"number is multiple of 5");
+
+} else{
+    console.log(num,"number is not multiple of 5");
 }
-console.log(profile);
